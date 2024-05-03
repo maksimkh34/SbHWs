f829c3199d72718d1321c3a0fa6b5be2d964cf0d
maksimkh34 <maksimkh34@duck.com>
2024-05-03 22:38:32 +0300

    hw 1

---
+++ HW1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DotnetSkillboxC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+            // Task2();
+        }
+
+        static void Task1() {
+            Console.WriteLine("Hello World!!!");
+
+            Console.ReadKey();
+        }
+
+        static void Task2() {
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            
+            Console.ReadKey();
+        }
+    }
+}
