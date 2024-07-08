3493e3c7f81c8637a3a16e40bf7e392bc3bd0fb2
maksimkh34 <maksimkh34@duck.com>
2024-07-08 11:55:21 +0300

    HW5

---
+++ HW5/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace HW5
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Task2.TaskMain();
+        }
+    }
+
+    public static class Task1
+    {
+        public static void TaskMain()
+        {
+            Console.WriteLine("Enter your text:");
+            var text = Console.ReadLine();
+            var words = SplitText(text ?? string.Empty);
+            PrintWords(words);
+        }
+
+        private static string[] SplitText(string text) => text.Split(' ');
+
+        private static void PrintWords(string[] words)
+        {
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+
+    public static class Task2
+    {
+
+        public static void TaskMain()
+        {
+            Console.WriteLine("Enter your text:");
+            var text = Console.ReadLine();
+            Console.WriteLine(Reverse(text ?? string.Empty));
+        }
+
+        private static string Reverse(string text)
+        {
+            var words = SplitText(text);
+            var len = words.Length;
+
+            for (var i = 0; i < len / 2; i++)
+            {
+                (words[len - i - 1], words[i]) = (words[i], words[len - i - 1]);
+            }
+
+            return string.Join(" ", words);
+        }
+
+        private static string[] SplitText(string text) => text.Split(' ');
+    }
+}
